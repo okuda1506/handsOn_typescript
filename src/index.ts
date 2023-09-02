edb795845fa2d4f01a1f1fd1a99b51d2bcde4cab
okuda1506 <okuda1506@gmail.com>
2023-09-02 14:08:29 +0900

    BigIntとnumber型

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 const bignum: bigint = (123n + 456n) * 2n;
 
+const result = 5n / 2n;
+
+// BigInt と number は一緒に使えないので下記はコンパイルエラー
+// const wrong = 100n + 50;
+
 console.log(bignum);
+console.log(result);
