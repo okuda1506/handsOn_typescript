363b82c0ee6e95e2a15eeb2c1cb8054a5604ddee
okuda1506 <okuda1506@gmail.com>
2023-09-02 15:04:30 +0900

    演算子（== と=== の違い）

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,10 @@
-import { createInterface } from "readline";
+const str: any = "3";
 
-const rl = createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+// 暗黙の型変換を行ってから比較するためtrue
+console.log(str == 3);
 
-rl.question('数値を入力してください: ', (line) => {
-    // 明示的な変換
-    const num = Number(line);
-    console.log(num + 1000);
-    rl.close();
-});
+// 両オペランドの型が異なる場合は常にfalse
+// 基本的に ===, !== を使うべき
+console.log(str === 3);
+
+// x == null の比較は == で良い ⇒ x がnull または undefined の意味
