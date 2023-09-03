b2069728811cfc80bb6a416f3c87155500688e01
okuda1506 <okuda1506@gmail.com>
2023-09-03 14:43:08 +0900

    真偽値演算（falseのときのみ処理を行う）

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
-const str: any = "3";
+const num: number = NaN;
 
-// 暗黙の型変換を行ってから比較するためtrue
-console.log(str == 3);
-
-// 両オペランドの型が異なる場合は常にfalse
-// 基本的に ===, !== を使うべき
-console.log(str === 3);
-
-// x == null の比較は == で良い ⇒ x がnull または undefined の意味
+if (!Number.isNaN(num)) {
+    console.log(`${num}はNaNではありません`);
+} else {
+    console.log(`${num}はNaNです`);
+}
