c0c1f67f8d1aa292f0a6e34847f9ad7659a69c62
okuda1506 <okuda1506@gmail.com>
2023-09-03 15:01:39 +0900

    論理演算子（!!式 の意味）

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 const input1 = "123", input2 = "";
 
-// !! ⇒オペランドを真偽値に変換(Boolean() と同じ意味)
+//!! の意味⇒!(!式)という意味 
+// !! ⇒オペランドを真偽値に変換(Boolean() と同じ)
 const input1isNotEmpty = !!input1;
 console.log(input1isNotEmpty); // true
 const input2isNotEmpty = !!input2;
