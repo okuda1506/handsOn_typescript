8192640c0f2fe0b8f4531cece925c1d5c0f0bf2d
okuda1506 <okuda1506@gmail.com>
2023-09-03 15:43:00 +0900

    条件演算子

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
-// 環境変数SECRETを取得。存在しなければ"default"を用いる
-const secret = process.env.SECRET ?? "default";
+import { createInterface } from "readline";
 
-console.log(`secretは${secret}です`);
+const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('数値を入力してください:', (line) => {
+    const num = Number(line);
+    const message = 0 <= num && num < 100
+        ? `${num}は0以上100未満です`
+        : `${num}は0以上100未満ではありません`;
+    console.log(message);
+    rl.close();
+});
