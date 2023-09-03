af2f2514a726b053ed3ab4a3d65474ffcc624323
okuda1506 <okuda1506@gmail.com>
2023-09-03 14:57:47 +0900

    論理演算子（!!式 について）

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-const num: number = NaN;
+const input1 = "123", input2 = "";
 
-if (!Number.isNaN(num)) {
-    console.log(`${num}はNaNではありません`);
-} else {
-    console.log(`${num}はNaNです`);
-}
+// !! ⇒オペランドを真偽値に変換(Boolean() と同じ意味)
+const input1isNotEmpty = !!input1;
+console.log(input1isNotEmpty); // true
+const input2isNotEmpty = !!input2;
+console.log(input2isNotEmpty); // false
